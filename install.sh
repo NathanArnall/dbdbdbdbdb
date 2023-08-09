334bf94a4087313fc0c0ea5be9a45297db89df85
NathanArnall <90006919+NathanArnall@users.noreply.github.com>
2023-08-09 16:36:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 mkdir dbedition
 cd dbedition
 curl https://github.com/NathanArnall/dbdbdbdbdb/blob/main/createfiles.sh > createfiles.sh
+curl https://github.com/NathanArnall/dbdbdbdbdb/blob/main/db.py > dp.py.sh
