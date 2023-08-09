1fe53b549c8fa1aea34cd190bf49896c2955595e
NathanArnall <90006919+NathanArnall@users.noreply.github.com>
2023-08-09 16:55:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir dbedition
 cd dbedition
-curl https://github.com/NathanArnall/dbdbdbdbdb/blob/main/createfiles.sh > createfiles.sh
+curl https://raw.githubusercontent.com/NathanArnall/dbdbdbdbdb/main/createfiles.sh?token=GHSAT0AAAAAACGDTECTSQMZEX63LJW5GCZUZGUBN5A > createfiles.sh
 curl https://raw.githubusercontent.com/NathanArnall/dbdbdbdbdb/main/db.py?token=GHSAT0AAAAAACGDTECS3WHPR2JZ4SLQRNZUZGUBNNA > dp.py
 ./createfiles.sh
 python3 db.py
