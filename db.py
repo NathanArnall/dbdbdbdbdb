2479a02196e14276080dbe9c16d80ece77cdf52d
NathanArnall <90006919+NathanArnall@users.noreply.github.com>
2023-08-09 17:07:20 -0500

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,11 @@ pngfile = open('pnglist.txt', 'r')
 pngreads = pngfile.readlines()
 for png in pngreads:
         print('Purifying ' + png)
-        os.popen('cp /pics/db.png ' + png)
+        os.popen('cp /~/dbedition/db.png ' + png)
 pngfile.close()
 jpgfile = open('jpglist.txt', 'r')
 jpgreads = jpgfile.readlines()
 for jpg in jpgreads:
         print('Purifying ' + jpg)
-        os.popen('cp /pics/db.jpg ' + jpg)
+        os.popen('cp /~/dbedition/db.jpg ' + jpg)
 jpgfile.close()
