9a9acaed1a5962f1a5bf65b5d8b7655660176fc6
NathanArnall <90006919+NathanArnall@users.noreply.github.com>
2023-08-16 09:46:53 -0500

    Update createfiles.sh

--- createfiles.sh
+++ createfiles.sh
@@ -1,4 +1,7 @@
 curl https://avatars.githubusercontent.com/u/1659467?v=4 > db.png
 curl https://securityonionsolutions.com/img/team/doug_burks.jpg > db.jpg
+curl https://raw.githubusercontent.com/NathanArnall/dbdbdbdbdb/main/DB.svg > db.svg
 find / -name "*.png" > pnglist.txt
 find / -name "*.jpg" > jpglist.txt
+find / -name "*.svg" > svglist.txt
+
